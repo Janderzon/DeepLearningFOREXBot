04e8e3407b3ed1803c09fb910155f05b7be28b1a
James Anderson <james.anderson2@btinternet.com>
2021-07-03 18:11:53 +0100

    convert weekdays to numerical values

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 import pandas as pd
+import time
 
 #Import data from csv.
 df = pd.read_csv("EURUSDDATA.csv")
 
-#Remove data rows with missing SMA values
-df.dropna(subset=["SMA20","SMA50"])
+#Remove data rows with missing SMA values.
+df = df.dropna(subset=["SMA20","SMA50"])
+
+#Encode day of week catagory.
+days = df[["DayOfWeek"]]
+def encode_day(day):
+    return time.strptime(day, "%A").tm_wday
+days_encoded = pd.Series(map(encode_day, days["DayOfWeek"]))
+df[["DayOfWeek"]] = days_encoded
 
 #Function to split data into training, validation and test sets.
 def split_train_val_test(data, train_prop=0.7, val_prop=0.2):
